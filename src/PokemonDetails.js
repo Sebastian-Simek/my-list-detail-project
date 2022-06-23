05e1a361435a0e4c62ec4189a41cae5726dca082
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-06-23 15:13:13 -0700

    added some styling

--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -6,11 +6,10 @@ import { getPokemonById } from './services/fetch-utils.js';
 export default function PokemonDetails() {
   const [pokemon, setPokemon] = useState({});
   const params = useParams();
-
   useEffect(() => {
+
     async function getPokemonData() {
       const data = await getPokemonById(params.id);
-      console.log(data);
       setPokemon(data);
     }
 
@@ -19,6 +18,22 @@ export default function PokemonDetails() {
   
 
   return (
-    <div>Hello from PokemonDetails</div>
+    <div className='pokemon-detail-box'>
+      <h3>{pokemon.name}</h3>
+      <img src={pokemon.gif} width='200px'/>
+      <div className='stats'>
+        <p>Type:</p>
+        <p> {pokemon.type1} {`, ${pokemon.type2}`}</p>
+        <ul> Stats:
+          <li>Attack: {pokemon.attack}</li>
+          <li>Defense: {pokemon.defense}</li>
+          <li>Speed: {pokemon.speed}</li>
+          <li>Special Attack: {pokemon.special}</li>
+        </ul>
+      </div>
+      <div className='description'>
+        <p>{pokemon.description}</p>
+      </div>
+    </div>
   );
 }
